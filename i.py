ca24f5560cbcfb04034877e53e1d6c57d11ddc5f
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2023-12-06 13:41:20 +0500

    first commit

--- i.py
+++ i.py
@@ -1,2 +1,11 @@
-import os
-print("I have debug the code,Alhumdulilah")
+import pyttsx3
+
+if __name__ == '__main__':
+    text_to_speech = pyttsx3.init()
+    print("Welcome to Robo-Talk 1.0 Created by Abdul Wadood")
+    while(True):
+        word = input("Enter your command: ")
+        if word == 'q':
+            break
+        text_to_speech.say(word)
+        text_to_speech.runAndWait()
